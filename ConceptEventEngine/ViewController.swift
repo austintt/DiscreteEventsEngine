2432a7b48ba35fb4c3a388ca27b03c0899d36d79
Austin Tooley <tooley7@gmail.com>
2014-10-28 09:43:41 -0600

    Initial Commit

---
+++ ConceptEventEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConceptEventEngine
+//
+//  Created by Austin Tooley on 10/28/14.
+//  Copyright (c) 2014 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ConceptEventEngineTests/ConceptEventEngineTests.swift
@@ -0,0 +1,36 @@
+//
+//  ConceptEventEngineTests.swift
+//  ConceptEventEngineTests
+//
+//  Created by Austin Tooley on 10/28/14.
+//  Copyright (c) 2014 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ConceptEventEngineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
